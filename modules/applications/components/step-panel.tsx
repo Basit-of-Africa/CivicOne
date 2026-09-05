e244fe3496e2b92458de579948539a72e92a0fff
basit-of-africa <ajibadebasit40@gmail.com>
2026-09-05 13:49:45 +0100

    feat: import CacTinLookup component in StepPanel for CAC TIN functionality

--- modules/applications/components/step-panel.tsx
+++ modules/applications/components/step-panel.tsx
@@ -24,6 +24,7 @@ import {
   type FormDefinition,
   type VerifiedIdentity,
 } from "@/modules/applications/form-config";
+import { CacTinLookup } from "./cac-tin-lookup";
 
 export function StepPanel({
   application,
